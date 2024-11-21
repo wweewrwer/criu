61874a87e1a987b874244e4850df4fcd36d704c1
Yanning Yang <yangyanning@sjtu.edu.cn>
2024-11-21 13:24:12 +0000

    plugins/amdgpu: Add parallel restore command
    Currently the restore of buffer object comsumes a significant amount of
    time. However, this part has no logical dependencies with other restore
    operations. This patch introduce some structures and some helper
    functions for the target process to offload this task to the main CRIU
    process.
    
    Signed-off-by: Yanning Yang <yangyanning@sjtu.edu.cn>

--- plugins/amdgpu/amdgpu_socket_utils.c
+++ plugins/amdgpu/amdgpu_socket_utils.c
@@ -10,6 +10,7 @@
 
 int addr_len;
 struct sockaddr_un sock_addr;
+parallel_restore_cmd restore_cmd;
 int sock_id = 0;
 
 static void amdgpu_socket_name_gen(struct sockaddr_un *addr, int *len)
@@ -37,6 +38,161 @@ int install_parallel_sock()
 		ret = -1;
 		goto err;
 	}
+err:
+	close(sock_fd);
+	return ret;
+}
+
+void parallel_restore_bo_add(int dmabuf_fd, int gpu_id, uint64_t size, uint64_t offset, int minor)
+{
+	parallel_restore_entry *restore_entry = &restore_cmd.entries[restore_cmd.cmd_head.entry_num];
+	restore_entry->gpu_id = gpu_id;
+	restore_entry->minor = minor;
+	restore_entry->write_id = restore_cmd.cmd_head.fd_write_num;
+	restore_entry->write_offset = 0;
+	restore_entry->read_offset = offset;
+	restore_entry->size = size;
+
+	restore_cmd.fds_write[restore_cmd.cmd_head.fd_write_num] = dmabuf_fd;
+
+	restore_cmd.cmd_head.entry_num += 1;
+	restore_cmd.cmd_head.fd_write_num += 1;
+}
+
+int send_metadata(int sock_fd)
+{
+	if (sendto(sock_fd, &restore_cmd.cmd_head, sizeof(parallel_restore_cmd_head), 0, (struct sockaddr *)&sock_addr, addr_len) < 0) {
+		pr_perror("write");
+		return -1;
+	}
+	return 0;
+}
+
+int send_cmds(int sock_fd)
+{
+	if (sendto(sock_fd, restore_cmd.entries, restore_cmd.cmd_head.entry_num * sizeof(parallel_restore_entry), 0, (struct sockaddr *)&sock_addr, addr_len) < 0) {
+		pr_perror("write");
+		return -1;
+	}
+	return 0;
+}
+
+int send_dmabuf_fds(int sock_fd)
+{
+	if (send_fds(sock_fd, &sock_addr, addr_len, restore_cmd.fds_write, restore_cmd.cmd_head.fd_write_num, 0, 0) < 0) {
+		pr_perror("Send dmabuf fail");
+		return -1;
+	}
+	return 0;
+}
+
+int send_parallel_restore_cmd(void)
+{
+	int sock_fd;
+	int ret = 0;
+
+	if ((sock_fd = fdstore_get(sock_id)) < 0) {
+		return -1;
+	}
+
+	pr_info("Parallel restore: begin to send cmd_head\n");
+	ret = send_metadata(sock_fd);
+	if (ret) {
+		goto err;
+	}
+	pr_info("Parallel restore: begin to send entries\n");
+	ret = send_cmds(sock_fd);
+	if (ret) {
+		goto err;
+	}
+	pr_info("Parallel restore: begin to send fds\n");
+	ret = send_dmabuf_fds(sock_fd);
+
+	pr_info("Parallel restore: connected over\n");
+
+err:
+	close(sock_fd);
+	return ret;
+}
+
+void init_parallel_restore_cmd(int num, int id)
+{
+	restore_cmd.cmd_head.id = id;
+	restore_cmd.cmd_head.fd_write_num = 0;
+	restore_cmd.cmd_head.entry_num = 0;
+
+	restore_cmd.fds_write = (int *)malloc(num * sizeof(int));
+	restore_cmd.entries = (parallel_restore_entry *)malloc(num * sizeof(parallel_restore_entry));
+
+	memset(restore_cmd.fds_write, 0, num * sizeof(int));
+	memset(restore_cmd.entries, 0, num * sizeof(parallel_restore_entry));
+}
+
+void free_parallel_restore_cmd()
+{
+	free(restore_cmd.fds_write);
+	free(restore_cmd.entries);
+}
+
+void init_parallel_restore_cmd_by_head()
+{
+	restore_cmd.fds_write = (int *)malloc(restore_cmd.cmd_head.fd_write_num * sizeof(int));
+	restore_cmd.entries = (parallel_restore_entry *)malloc(restore_cmd.cmd_head.entry_num * sizeof(parallel_restore_entry));
+}
+
+int recv_metadata(int client_fd)
+{
+	if (recvfrom(client_fd, &restore_cmd.cmd_head, sizeof(parallel_restore_cmd_head), 0, NULL, NULL) < 0) {
+		pr_perror("read");
+		return -1;
+	}
+	return 0;
+}
+
+int recv_cmds(int client_fd)
+{
+	if (recvfrom(client_fd, restore_cmd.entries, restore_cmd.cmd_head.entry_num * sizeof(parallel_restore_entry), 0, NULL, NULL) < 0) {
+		pr_perror("read");
+		return -1;
+	}
+	return 0;
+}
+
+int recv_dmabuf_fds(int client_fd)
+{
+	if (recv_fds(client_fd, restore_cmd.fds_write, restore_cmd.cmd_head.fd_write_num, 0, 0) < 0) {
+		pr_perror("Recv dmabuf fail");
+		return -1;
+	}
+	return 0;
+}
+
+int recv_parallel_restore_cmd()
+{
+	int sock_fd;
+	int ret = 0;
+
+	if ((sock_fd = fdstore_get(sock_id)) < 0)
+		return -1;
+
+	pr_info("Parallel restore: begin to recv cmd_head\n");
+	ret = recv_metadata(sock_fd);
+	if (ret) {
+		goto err;
+	}
+	init_parallel_restore_cmd_by_head();
+
+	pr_info("Parallel restore: begin to recv entries\n");
+	ret = recv_cmds(sock_fd);
+	if (ret) {
+		goto err;
+	}
+
+	pr_info("Parallel restore: begin to recv fds\n");
+	ret = recv_dmabuf_fds(sock_fd);
+
+	pr_info("Parallel restore: recv over\n");
+
 err:
 	close(sock_fd);
 	return ret;
--- plugins/amdgpu/amdgpu_socket_utils.h
+++ plugins/amdgpu/amdgpu_socket_utils.h
@@ -9,6 +9,38 @@
 #include <errno.h>
 #include <stdint.h>
 
+typedef struct {
+	int id;
+	int fd_write_num;
+	int entry_num;
+} parallel_restore_cmd_head;
+
+typedef struct {
+	int gpu_id;
+	int minor;
+	int write_id;
+	uint64_t read_offset;
+	uint64_t write_offset;
+	uint64_t size;
+} parallel_restore_entry;
+
+typedef struct {
+	parallel_restore_cmd_head cmd_head;
+	int *fds_write;
+	parallel_restore_entry *entries;
+} parallel_restore_cmd;
+
+extern parallel_restore_cmd restore_cmd;
+
+void init_parallel_restore_cmd(int num, int id);
+
+void free_parallel_restore_cmd();
+
 int install_parallel_sock();
 
+int send_parallel_restore_cmd(void);
+
+int recv_parallel_restore_cmd(void);
+
+void parallel_restore_bo_add(int dmabuf_fd, int gpu_id, uint64_t size, uint64_t offset, int minor);
 #endif
