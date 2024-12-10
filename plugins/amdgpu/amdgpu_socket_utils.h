1c5dbbede1cebeefba22cabe9a264149c0e8ea3c
Yanning Yang <yangyanning@sjtu.edu.cn>
2024-12-10 12:38:48 +0000

    plugins/amdgpu: Add parallel restore command
    Currently the restore of buffer object comsumes a significant amount of
    time. However, this part has no logical dependencies with other restore
    operations. This patch introduce some structures and some helper
    functions for the target process to offload this task to the main CRIU
    process.
    
    Signed-off-by: Yanning Yang <yangyanning@sjtu.edu.cn>

--- plugins/amdgpu/amdgpu_socket_utils.h
+++ plugins/amdgpu/amdgpu_socket_utils.h
@@ -9,6 +9,47 @@
 #include <errno.h>
 #include <stdint.h>
 
+typedef struct {
+	int id;
+	int fd_write_num;
+	int entry_num;
+	int gpu_num;
+} parallel_restore_cmd_head;
+
+typedef struct {
+	int gpu_id;
+	int minor;
+} parallel_gpu_info;
+
+typedef struct {
+	int gpu_id;
+	int write_id;
+	uint64_t read_offset;
+	uint64_t write_offset;
+	uint64_t size;
+} parallel_restore_entry;
+
+typedef struct {
+	parallel_restore_cmd_head cmd_head;
+	int *fds_write;
+	parallel_gpu_info *gpu_ids;
+	parallel_restore_entry *entries;
+} parallel_restore_cmd;
+
+int init_parallel_restore_cmd(int num, int id, int gpu_num, parallel_restore_cmd *restore_cmd);
+
+void free_parallel_restore_cmd(parallel_restore_cmd *restore_cmd);
+
 int install_parallel_sock(void);
 
+int send_parallel_restore_cmd(parallel_restore_cmd *restore_cmd);
+
+int recv_parallel_restore_cmd(parallel_restore_cmd *restore_cmd);
+
+void parallel_restore_bo_add(int dmabuf_fd, int gpu_id, uint64_t size, uint64_t offset,
+			     parallel_restore_cmd *restore_cmd);
+
+void parallel_restore_gpu_id_add(int gpu_id, int minor, parallel_restore_cmd *restore_cmd);
+
+int close_parallel_restore_server(void);
 #endif
